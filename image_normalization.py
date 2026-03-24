bd208e08dc197fb98f120b9b5d938be9e276bb47
Liu317Moku <s1058001@gm.csmu.edu.tw>
2026-03-24 11:18:19 +0800

    Update image_normalization.py

--- image_normalization.py
+++ image_normalization.py
@@ -3,7 +3,7 @@ import shutil
 import glob
 
 #nU-Net 資料夾
-task_dir = "/home/u20030317/twcc1/nnunet/nnUNet_raw/Dataset001" #改成自己的帳號名稱放置路徑中
+task_dir = "/home/nnunet/nnUNet_raw/Dataset001" #改成自己的帳號名稱放置路徑中
 imagesTr_dir = os.path.join(task_dir, "imagesTr")
 labelsTr_dir = os.path.join(task_dir, "labelsTr")
 imagesTs_dir = os.path.join(task_dir, "imagesTs")
@@ -12,8 +12,8 @@ os.makedirs(labelsTr_dir, exist_ok=True)
 os.makedirs(imagesTs_dir, exist_ok=True)
 
 #原始影像與 mask 資料夾
-src_images = "/home/u20030317/ICTS/2024/train/med-ddpm/image" #改成自己的帳號名稱放置路徑中
-src_masks = "/home/u20030317/ICTS/2024/train/med-ddpm/mask" #改成自己的帳號名稱放置路徑中
+src_images = "/home/train/med-ddpm/image" #改成自己的帳號名稱放置路徑中
+src_masks = "/home/train/med-ddpm/mask" #改成自己的帳號名稱放置路徑中
 
 # 找出所有影像檔
 image_paths = sorted(glob.glob(os.path.join(src_images, "*")))
@@ -33,4 +33,4 @@ for idx, (img_path, mask_path) in enumerate(zip(image_paths, mask_paths)):
     shutil.copy(img_path, os.path.join(imagesTr_dir, new_img_name))
     shutil.copy(mask_path, os.path.join(labelsTr_dir, new_mask_name))
 
-print(f"✅ 已完成轉換並複製 {len(image_paths)} 筆資料到 nnU-Net 格式資料夾：{task_dir}")
+print(f" 已完成轉換並複製 {len(image_paths)} 筆資料到 nnU-Net 格式資料夾：{task_dir}")
